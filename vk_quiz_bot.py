c4461f2a91963063251ad635aa2a05f324b546e7
Sergey Markov <107784915+SrgMarkov@users.noreply.github.com>
2024-01-06 22:06:03 +0400

    add more functions to vk_bot

--- vk_quiz_bot.py
+++ vk_quiz_bot.py
@@ -15,13 +15,30 @@ from questions import get_questions
 logger = logging.getLogger('QUIZ_bot VK')
 
 
-def get_vk_keyboard():
+def reply_from_vk_bot(user_id, message):
     keyboard = VkKeyboard()
     keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
     keyboard.add_button('Сдаться', color=VkKeyboardColor.SECONDARY)
     keyboard.add_line()
     keyboard.add_button('Мой счёт', color=VkKeyboardColor.SECONDARY)
-    return keyboard.get_keyboard()
+
+    vk_session.method('messages.send', {
+        'user_id': user_id,
+        'message': message,
+        'random_id': get_random_id(),
+        'keyboard': keyboard.get_keyboard(),
+    })
+
+
+def get_answer_from_user(event, user_id, redis_db):
+    if event.text.lower() in redis_db.get(user_id).decode().lower() and len(event.text) > 1:
+        logger.info(f'Пользователь {user_id} - Получен верный ответ')
+        user_result = redis_db.get(f'{user_id}_result') + 1
+        redis_db.set(f'{user_id}_result', user_result)
+        reply_from_vk_bot(user_id, 'Правильно! Поздравляю! Для следующего вопроса нажми "Новый вопрос"')
+    else:
+        logger.info(f'Пользователь {user_id} - Получен не верный ответ')
+        reply_from_vk_bot(user_id, 'Неправильно… Попробуешь ещё раз?')
 
 
 if __name__ == "__main__":
@@ -29,59 +46,30 @@ if __name__ == "__main__":
     vk_session = vk.VkApi(token=os.getenv('VK_TOKEN'))
     redis_db = redis.Redis(host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'))
 
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
     logger.setLevel(logging.INFO)
 
     questions = get_questions()
-    user_results = 0
 
     for event in VkLongPoll(vk_session).listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             user_id = event.user_id
+            if redis_db.get(f'{user_id}_result') is None:
+                redis_db.set(f'{user_id}_result', 0)
             if event.text == 'Новый вопрос':
                 logger.info(f'Пользователь {user_id} - Запрошен новый вопрос')
                 question = random.choice(questions)
                 redis_db.set(user_id, question['answer'])
-                vk_session.method('messages.send', {
-                    'user_id': user_id,
-                    'message': question['question'],
-                    'random_id': get_random_id(),
-                    'keyboard': get_vk_keyboard(),
-                })
+                reply_from_vk_bot(user_id, question['question'])
             elif event.text == 'Сдаться':
                 logger.info(f'Пользователь {user_id} - Сдался')
-                vk_session.method('messages.send', {
-                    'user_id': user_id,
-                    'message': f'Вот тебе правильный ответ - {redis.get(user_id).decode()} Чтобы продолжить - нажми '
-                               f'"Новый вопрос"',
-                    'random_id': get_random_id(),
-                    'keyboard': get_vk_keyboard(),
-                })
+                message = f'Вот тебе правильный ответ - {redis_db.get(user_id).decode()} Чтобы продолжить - нажми ' \
+                          f'"Новый вопрос"'
+                reply_from_vk_bot(user_id, message)
             elif event.text == 'Мой счёт':
                 logger.info(f'Пользователь {user_id} - запрошен счет')
-                vk_session.method('messages.send', {
-                    'user_id': user_id,
-                    'message': f'Количество правильных ответов - {user_results}',
-                    'random_id': get_random_id(),
-                    'keyboard': get_vk_keyboard(),
-                })
+                message = f'Количество правильных ответов - {redis_db.get(f"{user_id}_result").decode()}'
+                reply_from_vk_bot(user_id, message)
             else:
-                if event.text.lower() in redis.get(user_id).decode().lower() and len(event.text) > 1:
-                    logger.info(f'Пользователь {user_id} - Получен верный ответ')
-                    user_results += 1
-                    vk_session.method('messages.send', {
-                        'user_id': user_id,
-                        'message': 'Правильно! Поздравляю! Для следующего вопроса нажми "Новый вопрос"',
-                        'random_id': get_random_id(),
-                        'keyboard': get_vk_keyboard(),
-                    })
-                else:
-                    logger.info(f'Пользователь {user_id} - Получен не верный ответ')
-                    vk_session.method('messages.send', {
-                        'user_id': user_id,
-                        'message': 'Неправильно… Попробуешь ещё раз?',
-                        'random_id': get_random_id(),
-                        'keyboard': get_vk_keyboard(),
-                    })
+                get_answer_from_user(event, user_id, redis_db)
 
